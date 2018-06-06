173e2b50650b4f5f11cfef9258a1fa5e5e04672c
Shubham Shukla <snkshukla@gmail.com>
2018-06-06 17:47:07 +0530

    Add sidekiq config and update the deployment and service configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
 
   root 'welcome#index'
+
+  mount Sidekiq::Web => '/kiq'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
